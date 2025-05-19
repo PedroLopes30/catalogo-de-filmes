a5620ecc6eceb0cdb4a7ff1b724aeffcf217c69c
PedroLopes30 <pedrohenriquelds1506@gmail.com>
2025-05-19 10:25:14 -0300

    branch filmes criada

---
+++ data control/movies.py
@@ -0,0 +1,6 @@
+class Filmes():
+    def __init__(self, nome, data_de_lancamento, genero, avaliacao):
+        self.nome = nome 
+        self.data_de_lancamento = data_de_lancamento
+        self.genero = genero
+        self.avaliacao = avaliacao    
